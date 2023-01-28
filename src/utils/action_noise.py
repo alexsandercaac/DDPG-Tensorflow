ae24f3d23285b39fd8a5cd2f3e6f639c396193b2
Alexsander <alexsandercaac@gmail.com>
2023-01-28 11:44:01 -0300

    Added implementation of OU action noise.

--- src/utils/action_noise.py
+++ src/utils/action_noise.py
@@ -6,6 +6,7 @@
 """
 import abc
 import numpy as np
+from typing import Optional
 
 
 class ActionNoise(metaclass=abc.ABCMeta):
@@ -27,3 +28,62 @@ class ActionNoise(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def __call__(self) -> np.ndarray:
         raise NotImplementedError()
+
+
+class OUActionNoise(ActionNoise):
+    """
+        An Ornstein Uhlenbeck action noise, this is designed to approximate
+        Brownian motion with friction. Noise is sampled from a correlated
+        normal distribution.
+
+        Implementation based on:
+        https://github.com/hill-a/stable-baselines/blob/master/stable_baselines/common/noise.py
+
+        Args:
+
+        mean (ndarray): the mean of the noise
+        sigma (ndarray): the scale of the noise
+        theta (ndarray): the rate of mean reversion. Default: 0.15.
+        dt (float): the timestep for the noise. Default: 1e-2.
+        initial_noise (ndarray): the initial value for the noise output.
+            Default: None (0).
+
+    """
+
+    def __init__(
+            self,
+            mean: np.ndarray,
+            sigma: np.ndarray,
+            theta: float = 0.15,
+            dt: float = 1e-2,
+            initial_noise: Optional[np.ndarray] = None):
+
+        super(OUActionNoise, self).__init__()
+        self._theta = theta
+        self._mu = mean
+        self._sigma = sigma
+        self._dt = dt
+        self.initial_noise = initial_noise
+        self.noise_prev = np.zeros_like(self._mu)
+        self.reset()
+
+    def __call__(self) -> np.ndarray:
+        noise = (
+            self.noise_prev
+            + self._theta * (self._mu - self.noise_prev) * self._dt
+            + self._sigma * np.sqrt(self._dt) *
+            np.random.normal(size=self._mu.shape)
+        )
+        self.noise_prev = noise
+        return noise
+
+    def reset(self) -> None:
+        """
+        Reset the Ornstein Uhlenbeck noise to the initial position
+        """
+        self.noise_prev = self.initial_noise if self.initial_noise is not None\
+            else np.zeros_like(self._mu)
+
+    def __repr__(self) -> str:
+        return (f"OrnsteinUhlenbeckActionNoise(mu={self._mu}, " +
+                f"sigma={self._sigma})")
