b1f28d8c7f25cc8784bad688c20d591f1a9d4565
Alexsander <alexsandercaac@gmail.com>
2023-01-31 12:16:38 -0300

    Remove batch normalization from the models

--- src/utils/anns.py
+++ src/utils/anns.py
@@ -20,15 +20,12 @@ def actor(num_states: int, num_actions: int, lr: float = 1e-3
     """
 
     inputs = tf.keras.layers.Input(shape=(num_states))
-    inputs_bnorm1 = tf.keras.layers.BatchNormalization()(inputs)
 
-    dense1 = tf.keras.layers.Dense(400, activation="linear")(inputs_bnorm1)
-    bnorm1 = tf.keras.layers.BatchNormalization()(dense1)
-    act1 = tf.keras.layers.Activation('relu')(bnorm1)
+    dense1 = tf.keras.layers.Dense(256, activation="linear")(inputs)
+    act1 = tf.keras.layers.Activation('relu')(dense1)
 
-    dense2 = tf.keras.layers.Dense(300, activation="linear")(act1)
-    bnorm2 = tf.keras.layers.BatchNormalization()(dense2)
-    act2 = tf.keras.layers.Activation('relu')(bnorm2)
+    dense2 = tf.keras.layers.Dense(256, activation="linear")(act1)
+    act2 = tf.keras.layers.Activation('relu')(dense2)
 
     last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
     outputs = tf.keras.layers.Dense(num_actions, activation="tanh",
@@ -57,23 +54,32 @@ def critic(num_states: int, num_actions: int, lr: float = 1e-3
 
     # State as input
     state_input = tf.keras.layers.Input(shape=(num_states))
-    state_bnorm1 = tf.keras.layers.BatchNormalization()(state_input)
     regularizer = tf.keras.regularizers.l2(1e-2)
-    state_dense1 = tf.keras.layers.Dense(400, activation="linear",
+    state_dense1 = tf.keras.layers.Dense(16, activation="linear",
                                          kernel_regularizer=regularizer
-                                         )(state_bnorm1)
-    state_bnorm2 = tf.keras.layers.BatchNormalization()(state_dense1)
-    state_act1 = tf.keras.layers.Activation('relu')(state_bnorm2)
+                                         )(state_input)
+    state_act1 = tf.keras.layers.Activation('relu')(state_dense1)
+    state_dense2 = tf.keras.layers.Dense(32, activation="linear",
+                                         kernel_regularizer=regularizer
+                                         )(state_act1)
+    state_act2 = tf.keras.layers.Activation('relu')(state_dense2)
 
     # Action as input
     action_input = tf.keras.layers.Input(shape=(num_actions))
+    action_dense1 = tf.keras.layers.Dense(32, activation="linear",
+                                          kernel_regularizer=regularizer
+                                          )(action_input)
+    action_act1 = tf.keras.layers.Activation('relu')(action_dense1)
 
-    concat = tf.keras.layers.Concatenate()([state_act1, action_input])
+    concat = tf.keras.layers.Concatenate()([state_act2, action_act1])
 
-    dense1 = tf.keras.layers.Dense(300, activation="relu",
+    dense1 = tf.keras.layers.Dense(256, activation="relu",
                                    kernel_regularizer=regularizer)(concat)
+    dense2 = tf.keras.layers.Dense(256, activation="relu",
+                                   kernel_regularizer=regularizer)(dense1)
     last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
-    outputs = tf.keras.layers.Dense(1, kernel_initializer=last_init)(dense1)
+
+    outputs = tf.keras.layers.Dense(1, kernel_initializer=last_init)(dense2)
 
     model = tf.keras.Model([state_input, action_input], outputs)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr))
